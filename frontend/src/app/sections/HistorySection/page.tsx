9c9534be95b8ffd1962937be6cf65691d66b1ef1
Rijan Dahal <reasondahal719@gmail.com>
2026-03-08 16:43:29 +0545

    Fix: Missing Deerhack 25 Legacy Section

--- frontend/src/app/sections/HistorySection/page.tsx
+++ frontend/src/app/sections/HistorySection/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import WinnersOf23 from "./winners/winnersOf23";
 import WinnersOf24 from "./winners/winnersOf24";
+import WinnersOf25 from "./winners/winnersOf25";
 
 const HistorySection = () => {
     
@@ -9,6 +10,7 @@ const HistorySection = () => {
         <div className="flex flex-col lg:gap-[15rem] gap-[5rem]  overflow-hidden w-full">
             <WinnersOf23 /> 
             <WinnersOf24 /> 
+            <WinnersOf25 /> 
         </div>
     </>
 
