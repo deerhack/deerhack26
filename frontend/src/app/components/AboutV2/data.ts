56c37f8f9702077ed66c55e572133e632fd09f88
Rijan Dahal <reasondahal719@gmail.com>
2026-03-22 17:24:58 +0545

    style: made code prettier

--- frontend/src/app/components/AboutV2/data.ts
+++ frontend/src/app/components/AboutV2/data.ts
@@ -2,25 +2,23 @@ import CollegeSVG from "@/app/assets/icons/College";
 import CodeSVG from "@/app/assets/icons/Code";
 import ClubSVG from "@/app/assets/icons/ClubSVG";
 
-
 export const details = [
-    {
-        svg: CodeSVG,
-        id: 1,
-        title: "Deerhack26",
-        desc: 
-            "DeerHack 2024 is a 48-hour hackathon where developers, designers, students, entrepreneurs, educators, and mentors work together to build ideas that solve real-world complications. DeerHack provides an opportunity for individuals to sharpen their teamwork, communication, leadership skills, and networking in a risk-free and comfortable environment. It serves as a collaborative hub where creativity meets technical execution, pushing participants to step outside their comfort zones and transform abstract concepts into functional prototypes. Beyond the competitive spirit, the event fosters a culture of mentorship and peer learning, allowing hackers to bridge the gap between academic theory and industry practice."
-    },
-    {
-        svg: ClubSVG, 
-        id: 2,
-        title: "Software Club",
-        desc: "The Software Club of Deerwalk College organizes DeerHack, a 48-hour hackathon that brings together developers, designers, students, entrepreneurs, educators, and mentors to collaborate on innovative ideas that address real world challenges. Through DeerHack, the club creates a platform where participants can explore creativity, build practical solutions, and enhance their teamwork, communication, leadership, and networking skills in a supportive and collaborative environment. As a student-led initiative, the club focuses on cultivating a vibrant tech culture within the campus by bridging the gap between academic learning and industry standards."
-    },
-    {
-        svg: CollegeSVG,
-        id: 3,
-        title: "Deerwalk College",
-        desc: "Deerwalk Institute of Technology (DWIT) is an educational institution that prepares students and professionals of all levels to meet the problems and grab the possibilities of the twenty-first century IT Industry. The institute is well-known for its approach to learning that is both open-minded and responsible. It has an affiliation with Tribhuvan University and offers two undergraduate degrees, BSc. CSIT and BCA. By integrating rigorous technical training with a focus on soft skills and entrepreneurship, the college bridges the gap between theoretical education and the fast-paced demands of the global tech market. The campus serves as a vibrant ecosystem for innovation, hosting various student-led clubs and workshops that foster a culture of continuous learning and professional growth."
-    },
-]
+	{
+		svg: CodeSVG,
+		id: 1,
+		title: "Deerhack26",
+		desc: "DeerHack 2024 is a 48-hour hackathon where developers, designers, students, entrepreneurs, educators, and mentors work together to build ideas that solve real-world complications. DeerHack provides an opportunity for individuals to sharpen their teamwork, communication, leadership skills, and networking in a risk-free and comfortable environment. It serves as a collaborative hub where creativity meets technical execution, pushing participants to step outside their comfort zones and transform abstract concepts into functional prototypes. Beyond the competitive spirit, the event fosters a culture of mentorship and peer learning, allowing hackers to bridge the gap between academic theory and industry practice.",
+	},
+	{
+		svg: ClubSVG,
+		id: 2,
+		title: "Software Club",
+		desc: "The Software Club of Deerwalk College organizes DeerHack, a 48-hour hackathon that brings together developers, designers, students, entrepreneurs, educators, and mentors to collaborate on innovative ideas that address real world challenges. Through DeerHack, the club creates a platform where participants can explore creativity, build practical solutions, and enhance their teamwork, communication, leadership, and networking skills in a supportive and collaborative environment. As a student-led initiative, the club focuses on cultivating a vibrant tech culture within the campus by bridging the gap between academic learning and industry standards.",
+	},
+	{
+		svg: CollegeSVG,
+		id: 3,
+		title: "Deerwalk College",
+		desc: "Deerwalk Institute of Technology (DWIT) is an educational institution that prepares students and professionals of all levels to meet the problems and grab the possibilities of the twenty-first century IT Industry. The institute is well-known for its approach to learning that is both open-minded and responsible. It has an affiliation with Tribhuvan University and offers two undergraduate degrees, BSc. CSIT and BCA. By integrating rigorous technical training with a focus on soft skills and entrepreneurship, the college bridges the gap between theoretical education and the fast-paced demands of the global tech market. The campus serves as a vibrant ecosystem for innovation, hosting various student-led clubs and workshops that foster a culture of continuous learning and professional growth.",
+	},
+];
