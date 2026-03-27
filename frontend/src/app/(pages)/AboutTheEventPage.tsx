6750b19514f8f6feab7a00519266a452240bc64f
Rijan Dahal <reasondahal719@gmail.com>
2026-03-27 23:49:34 +0545

    add: animations to about card

--- frontend/src/app/(pages)/AboutTheEventPage.tsx
+++ frontend/src/app/(pages)/AboutTheEventPage.tsx
@@ -1,52 +1,102 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import AboutTheEventCards from "../sections/AboutTheEvent/AboutTheEventCards";
-import { cabinetExtraBold, cabinetMedium } from "@/fonts";
-import Image from "next/image";
-import about_right from "@/app/assets/images/AboutRight.svg";
-import about_left from "@/app/assets/images/AboutLeft.svg";
+import { cabinetExtraBold } from "@/fonts";
 import Laptop from "@/app/assets/icons/Laptop";
 import CoffeCup from "@/app/assets/icons/CoffeeCup";
 import DarinCup from "@/app/assets/icons/DarinCup";
 import Guitar from "@/app/assets/icons/Guitar";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+if (typeof window !== "undefined") {
+	gsap.registerPlugin(ScrollTrigger);
+}
+
 export default function Home() {
-  return (
-    <>
-      <div className="relative overflow-x-hidden overflow-y-hidden">
-        
-        <Laptop
-          height={306.0217203841847}
-          width={354.206822415178}
-          className="absolute -left-[5px] bottom-[8%]"
-        />
-        <CoffeCup
-          height={170.8614890732017}
-          width={124.23594985464511}
-          className="absolute left-[38px] bottom-[38%]"
-        />
-        
-        <DarinCup
-          height={600}
-          width={420}
-          className="absolute right-[-160px] bottom-[-10px]"
-        />
+	const container = useRef<HTMLDivElement>(null);
 
-        <div className="pb-[280px] pt-[100px]">
-          <h1
-            className={`${cabinetExtraBold.className} mb-[3rem] headings lg:text-[55px]`}
-          >
-            About The Event
-          </h1>
+	useGSAP(
+		() => {
+			const icons = gsap.utils.toArray<HTMLElement>(".event-icon");
+			icons.forEach((icon) => {
+				gsap.fromTo(
+					icon,
+					{ x: -600, rotate: -180, opacity: 0 },
+					{
+						x: 0,
+						rotate: 0,
+						duration: 1,
+						opacity: 1,
+						ease: "expo.out",
+						scrollTrigger: {
+							trigger: icon,
+							start: "top 95%",
+							toggleActions: "play none none reverse",
+							invalidateOnRefresh: true,
+						},
+					},
+				);
+			});
 
-          <AboutTheEventCards />
-        </div>
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: container.current,
+					start: "top 85%",
+					toggleActions: "play none none reverse",
+				},
+			});
 
-        <Guitar
-          height={381.94}
-          width={358.77}
-          className="absolute bottom-[-25px] left-[200px] z-50 pointer-events-none"
-        />
-      </div>
-    </>
-  );
+			tl.fromTo(
+				".darin-anim",
+				{ yPercent: 100, rotate: 20, opacity: 0 },
+				{
+					yPercent: 0,
+					rotate: 0,
+					opacity: 1,
+					duration: 1.5,
+					ease: "power2.out",
+				},
+			);
+		},
+		{ scope: container },
+	);
+
+	return (
+		<div ref={container} className="relative overflow-hidden">
+			<Laptop
+				height={306}
+				width={354}
+				className="event-icon absolute -left-[5px] bottom-[8%]"
+			/>
+			<CoffeCup
+				height={170}
+				width={124}
+				className="event-icon absolute left-[38px] bottom-[38%]"
+			/>
+
+			<DarinCup
+				height={600}
+				width={420}
+				className="darin-anim absolute right-[-160px] bottom-[-10px]"
+			/>
+
+			<div className="pb-[280px] pt-[100px]">
+				<h1
+					className={`${cabinetExtraBold.className} mb-[3rem] headings lg:text-[55px]`}
+				>
+					About The Event
+				</h1>
+				<AboutTheEventCards />
+			</div>
+
+			<Guitar
+				height={381}
+				width={358}
+				className="event-icon absolute bottom-[-25px] left-[200px] z-50 pointer-events-none"
+			/>
+		</div>
+	);
 }
