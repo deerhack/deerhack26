4539d3d438cdf052b58163670abff56f8c4a254f
Ishan Vaidya <165746811+VaidyaIshan@users.noreply.github.com>
2026-06-10 08:13:53 +0545

    Updated Schedule

--- frontend/src/app/sections/Schedule/page.tsx
+++ frontend/src/app/sections/Schedule/page.tsx
@@ -39,7 +39,7 @@ const ScheduleSection = () => {
                 },
                 {
                     time: "3:00 PM",
-                    title: "Snacks Time",
+                    title: "Snacks & College Visit",
                     venue: "Cafeteria & Courtyard",
                 },
                 {
