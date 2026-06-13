5ee0cf264f00570f2b662e04b98c2a8f3f1a2f6e
ConfusedKnight <aarushdangol2006@gmail.com>
2026-06-13 11:55:57 +0545

    fix: new build fix

--- frontend/src/app/components/Judges/data.ts
+++ frontend/src/app/components/Judges/data.ts
@@ -1,7 +1,4 @@
 import type Judges from "@/app/types/judges";
-import BhabukImg from "@/app/assets/images/judges/Bhabuk.png"
-import RikeshImg from "@/app/assets/images/judges/rikesh.png"
-import SwastikaImg from "@/app/assets/images/judges/Swastika.png"
 
 
 
@@ -9,21 +6,21 @@ import SwastikaImg from "@/app/assets/images/judges/Swastika.png"
 const Judges: Judges[] = [
   {
     name:"Bhabuk Singh Kunwar",
-    image: BhabukImg,
+    image: "https://github.com/ConfusedKnight/JudgesImages26/blob/main/Bhabuk.png?raw=true",
     linkedin_url:"https://www.linkedin.com/in/bhabuk-kunwar-58a82751/",
     position:"Director of Engineering",
     company: "Infinite Computer Solutions",
   },
     {
     name:"Rikesh Lal Shrestha",
-    image: RikeshImg,
+    image: "https://github.com/ConfusedKnight/JudgesImages26/blob/main/rikesh.png?raw=true",
     linkedin_url:"https://www.linkedin.com/in/rikesh-lal-shrestha-55b063132/",
     position:"Senior Engineering Manager",
     company: "Leapfrog Technology. Inc.",
   },
   {
     name:"Swastika K.C.",
-    image: SwastikaImg,
+    image: "https://github.com/ConfusedKnight/JudgesImages26/blob/main/Swastika.png?raw=true",
     linkedin_url:"https://www.linkedin.com/in/swastika-kc-84a56413b/",
     position:"Innovation Program Coordinator",
     company: "Youth Innovation Lab",
